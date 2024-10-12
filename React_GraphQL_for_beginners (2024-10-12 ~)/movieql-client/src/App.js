79ea2f3d538fa862c61dd449276f7aaac4877f14
yoots50 <202210947@sangmyung.kr>
2024-10-12 21:39:09 +0900

    2024-10-12 (<= #0.3)

---
+++ React_GraphQL_for_beginners (2024-10-12 ~)/movieql-client/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./routes/movies";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
