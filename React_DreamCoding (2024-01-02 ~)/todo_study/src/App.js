ccbfa6a86f85229f15cb2cac044ade4f1929ff83
yoots50 <202210947@sangmyung.kr>
2025-04-09 21:41:42 +0900

    2025-04-09 (<= #8.13)

--- React_DreamCoding (2024-01-02 ~)/todo_study/src/App.js
+++ React_DreamCoding (2024-01-02 ~)/todo_study/src/App.js
@@ -7,14 +7,14 @@ const filters = ["all", "active", "completed"];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
+    <>
       <Header
         filters={filters}
         filter={filter}
         onFilterChange={setFilter}
       />
       <TodoList filter={filter}/>
-    </div>
+    </>
   );
 }
 
--- React_DreamCoding (2024-01-02 ~)/todo_study/src/components/Header/Header.jsx
+++ React_DreamCoding (2024-01-02 ~)/todo_study/src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
+import styles from "./Header.module.css";
 export default function Header({ filters, filter, onFilterChange }) {
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
-            <button onClick={() => onFilterChange(value)}>{value}</button>
+            <button
+              className={`${styles.filter} ${
+                filter === value && styles.selected
+              }`}
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
           </li>
         ))}
       </ul>
